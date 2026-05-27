3ec1a3f1920f8d456333f3455357ffbdc6713675
RaRa1305 <rakshitcosmo13@gmail.com>
2026-05-27 22:28:19 +0530

    Added key detection and timers

--- src/main.cpp
+++ src/main.cpp
@@ -30,18 +30,152 @@ int main(int argc, char *argv[])
     bool quit = false;
     SDL_Event e;
 
-
     while (!quit)
     {
         while (SDL_PollEvent(&e))
         {
             if (e.type == SDL_EVENT_QUIT)
                 quit = true;
+
+            else if (e.type == SDL_EVENT_KEY_DOWN)
+            {
+                switch (e.key.key)
+                {
+                case SDLK_X:
+                    chip8.keypad[0] = 1;
+                    break;
+
+                case SDLK_1:
+                    chip8.keypad[1] = 1;
+                    break;
+
+                case SDLK_2:
+                    chip8.keypad[2] = 1;
+                    break;
+
+                case SDLK_3:
+                    chip8.keypad[3] = 1;
+                    break;
+
+                case SDLK_Q:
+                    chip8.keypad[4] = 1;
+                    break;
+
+                case SDLK_W:
+                    chip8.keypad[5] = 1;
+                    break;
+
+                case SDLK_E:
+                    chip8.keypad[6] = 1;
+                    break;
+
+                case SDLK_A:
+                    chip8.keypad[7] = 1;
+                    break;
+
+                case SDLK_S:
+                    chip8.keypad[8] = 1;
+                    break;
+
+                case SDLK_D:
+                    chip8.keypad[9] = 1;
+                    break;
+
+                case SDLK_Z:
+                    chip8.keypad[0xA] = 1;
+                    break;
+
+                case SDLK_C:
+                    chip8.keypad[0xB] = 1;
+                    break;
+
+                case SDLK_4:
+                    chip8.keypad[0xC] = 1;
+                    break;
+
+                case SDLK_R:
+                    chip8.keypad[0xD] = 1;
+                    break;
+
+                case SDLK_F:
+                    chip8.keypad[0xE] = 1;
+                    break;
+
+                case SDLK_V:
+                    chip8.keypad[0xF] = 1;
+                    break;
+                }
+            }
+            else if (e.type == SDL_EVENT_KEY_UP)
+            {
+                switch (e.key.key)
+                {
+                case SDLK_X:
+                    chip8.keypad[0] = 0;
+                    break;
+                case SDLK_1:
+                    chip8.keypad[1] = 0;
+                    break;
+                case SDLK_2:
+                    chip8.keypad[2] = 0;
+                    break;
+                case SDLK_3:
+                    chip8.keypad[3] = 0;
+                    break;
+                case SDLK_Q:
+                    chip8.keypad[4] = 0;
+                    break;
+                case SDLK_W:
+                    chip8.keypad[5] = 0;
+                    break;
+                case SDLK_E:
+                    chip8.keypad[6] = 0;
+                    break;
+                case SDLK_A:
+                    chip8.keypad[7] = 0;
+                    break;
+                case SDLK_S:
+                    chip8.keypad[8] = 0;
+                    break;
+                case SDLK_D:
+                    chip8.keypad[9] = 0;
+                    break;
+                case SDLK_Z:
+                    chip8.keypad[0xA] = 0;
+                    break;
+                case SDLK_C:
+                    chip8.keypad[0xB] = 0;
+                    break;
+                case SDLK_4:
+                    chip8.keypad[0xC] = 0;
+                    break;
+                case SDLK_R:
+                    chip8.keypad[0xD] = 0;
+                    break;
+                case SDLK_F:
+                    chip8.keypad[0xE] = 0;
+                    break;
+                case SDLK_V:
+                    chip8.keypad[0xF] = 0;
+                    break;
+                }
+            }
         }
 
         for (int i = 0; i < 10; i++)
         {
             chip8.cycle();
+        }
+
+        if (chip8.delayTimer > 0)
+        {
+            --chip8.delayTimer;
+        }
+
+        if (chip8.soundTimer > 0)
+        {
+            std::cout << "BEEP" << '\n';
+            --chip8.soundTimer;
         }
 
         for (int i = 0; i < 2048; i++)
