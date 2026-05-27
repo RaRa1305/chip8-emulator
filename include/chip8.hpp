d61e9431f55ad29753e102e92bd34e17fddd4961
RaRa1305 <rakshitcosmo13@gmail.com>
2026-05-27 15:41:17 +0530

    Chip-8 class declared in Header

---
+++ include/chip8.hpp
@@ -0,0 +1,25 @@
+#include <cstdint>
+#include <array>
+
+class Chip8 {
+public:
+    Chip8(); 
+
+    // Memory and registers
+    std::array<uint8_t, 4096> memory{};
+    std::array<uint8_t, 16> V{};
+    uint16_t I{};
+    uint16_t PC{};
+
+    // Stack and stack pointer
+    std::array<uint16_t, 16> stack{};
+    uint8_t sp{};
+
+    // Timers
+    uint8_t delayTimer{};
+    uint8_t soundTimer{};
+
+    // Interface
+    std::array<uint32_t, 64 * 32> display{};
+    std::array<uint8_t, 16> keypad{};
+};
